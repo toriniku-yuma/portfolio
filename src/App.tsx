3de1346070e1b7f2ab8c0487fa6eb90fee957668
toriniku-yuma <jasintoho@hotmail.co.jp>
2023-03-10 12:40:40 +0900

    実機での表示がおかしかったので修正

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div>
       <div className='flex justify-between fixed bg-base-100 z-10 w-full top-0 left-0'>
         <div className='md:text-9xl text-5xl font-bold drop-shadow'><span className='text-primary'>PORT</span><span className=' text-secondary'>FOLIO</span></div>
-        <div className='flex items-end md:right-[10%] right-[5%] relative'>
+        <div className='flex items-end md:right-[10%] relative'>
           {menulist.map((value,index)=>{
             return(
               <a key={index} href={`#${value}`}>
