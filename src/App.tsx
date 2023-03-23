ea46f7cbef194ec1fb849a05ede2584ba28513a9
toriniku-yuma <jasintoho@hotmail.co.jp>
2023-03-23 10:15:32 +0900

    Three.jsでブルームをかける

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,11 @@ function App() {
     <div>
       <div className='flex justify-between fixed bg-base-100 z-10 w-full top-0 left-0'>
         <div className='md:text-9xl text-5xl font-bold drop-shadow'><span className='text-primary'>PORT</span><span className=' text-secondary'>FOLIO</span></div>
-        <div className='flex items-end md:right-[10%] right-2 relative'>
+        <div className='flex items-center md:pr-10'>
           {menulist.map((value,index)=>{
             return(
               <a key={index} href={`#${value}`}>
-              <div className=" md:text-lg md:mx-3 mx-2">{value}</div>
+              <div className="md:mx-5 mx-2 btn btn-lg">{value}</div>
               </a>
             )
           })}
--- src/ThreeVRM.tsx
+++ src/ThreeVRM.tsx
@@ -1,18 +1,10 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import * as THREE from 'three';
 import { GLTFLoader,GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
 import { VRMLoaderPlugin } from '@pixiv/three-vrm';
-
-class PromiseGLTFLoader extends GLTFLoader {
-  promiseLoad(
-    url: string,
-    onProgress?: ((event: ProgressEvent<EventTarget>) => void) | undefined,
-  ) {
-    return new Promise<GLTF>((resolve, reject) => {
-      super.load(url, resolve, onProgress, reject)
-    })
-  }
-}
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
+import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass";
+import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass"
 
 export function ThreeVRM(){
     const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -22,24 +14,42 @@ export function ThreeVRM(){
 
     async function init() {
 
-      // サイズを指定
-      const width = window.innerWidth;
-      const height = window.innerHeight;
+      let width = window.innerWidth;
+      let height = window.innerHeight;
 
       // レンダラーを作成
       const renderer = new THREE.WebGLRenderer({
         canvas:canvasRef.current!,
         antialias: true,
-        alpha: true,
       });
-      renderer.setSize(width, height);
+      renderer.setSize(width, height,false);
+      renderer.clearColor();
 
       // シーンを作成
       const scene = new THREE.Scene();
+
+      scene.background = new THREE.Color("rgb(33%, 33%, 33%)");
     
       // カメラを作成
-      const camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
-      camera.position.set(0, 0, +1000);
+      const camera = new THREE.PerspectiveCamera(30, width / height, 0.1, 10000);
+      camera.position.set(0.2, 1.2, 0.7);
+
+      // リサイズイベント発生時に実行
+      window.addEventListener('resize', onResize);
+
+      function onResize() {
+        // サイズを取得
+        width = window.innerWidth;
+        height = window.innerHeight;
+
+        // レンダラーのサイズを調整する
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.setSize(width, height);
+
+        // カメラのアスペクト比を正す
+        camera.aspect = width / height;
+        camera.updateProjectionMatrix();
+      }
 
       const loader = new GLTFLoader();
   
@@ -48,51 +58,50 @@ export function ThreeVRM(){
         return new VRMLoaderPlugin(parser);
       });
       
-      loader.load(
-        // URL of the VRM you want to load
-        './test.vrm',
-      
-        // called when the resource is loaded
-        (gltf) => {
-          // retrieve a VRM instance from gltf
-          const vrm = gltf.userData.vrm;
-      
-          // add the loaded vrm to the scene
-          scene.add(vrm.scene);
-      
-          // deal with vrm features
-          console.log(vrm);
-        },
-      
-        // called while loading is progressing
-        (progress) => console.log('Loading model...', 100.0 * (progress.loaded / progress.total), '%'),
-      
-        // called when loading has errors
-        (error) => console.error(error),
-      );
-    
-    
-      // 平行光源
-      const directionalLight = new THREE.DirectionalLight(0xFFFFFF);
-      directionalLight.position.set(1, 1, 1);
-      // シーンに追加
-      scene.add(directionalLight);
+      const vrm = await loader.loadAsync(
+          "test.vrm",
+          // called while loading is progressing
+          (progress) => console.log('Loading model...', 100.0 * (progress.loaded / progress.total), '%')
+      )
+
+      scene.add(vrm.scene)
+
+      //camera.lookAt()
+
+      // 環境光源を作成
+      // new THREE.AmbientLight(色, 光の強さ)
+      const light = new THREE.AmbientLight(0xFFFFFF, 0.5);
+      scene.add(light);
+
+      const renderScene = new RenderPass( scene, camera );
+
+      const bloomPass = new UnrealBloomPass( new THREE.Vector2( width, height ), 1.5, 0.4, 0.85 );
+      bloomPass.threshold = 0;
+      bloomPass.strength = 0.7;
+      bloomPass.radius = 0.3;
+
+      const composer = new EffectComposer( renderer );
+      composer.addPass( renderScene );
+      composer.addPass( bloomPass );
+
+      //renderer.toneMappingExposure = Math.pow( 1.5 , 4.0 );
     
       tick();
     
       // 毎フレーム時に実行されるループイベントです
       function tick() {
         // メッシュを回転させる
-        //vrm.scene.rotation.y += 0.01;
+        vrm.scene.rotation.y += 0.01;
+
         // レンダリング
-        renderer.render(scene, camera);
+        composer.render();
     
         requestAnimationFrame(tick);
       }
     }
     },[])
     return(
-        <canvas id="myCanvas" ref={canvasRef}></canvas>
+        <canvas className="" id="myCanvas" ref={canvasRef}></canvas>
     )
 }
 
